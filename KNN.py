e15af4f7bf16d3b572ce7f120601483158e29131
Arpitha Rao <69826982+arpi5191@users.noreply.github.com>
2024-04-13 14:56:42 -0700

    Add files via upload
    Adding the data file and KNN file

---
+++ KNN.py
@@ -0,0 +1,93 @@
+# Import packages
+import math
+import pandas as pd
+
+# SplitData(): Splits the data into training and testing subsets (70-30)
+def SplitData(data, train_size):
+
+    # Shuffle the data
+    shuffled_data = data.sample(frac=1).reset_index(drop=True)
+
+    # Find the index up until which the training data will be extracted
+    trainInd = int(len(shuffled_data) * train_size)
+
+    # Obtain the training and testing data
+    trainData = shuffled_data[:trainInd]
+    testData = shuffled_data[trainInd:]
+
+    # Return the training and testing data
+    return trainData, testData
+
+# Normalization(): Normalize the data through standardization
+def Normalization(data):
+
+    # Iterate through the names and corresponding data for each feature
+    for feature_name, feature_data in data.items():
+        # Check if the feature is not id or diagnosis and if not apply the standardization
+        if feature_name != 'id' and feature_name != 'diagnosis':
+            # Find the mean of the feature
+            feature_mean = data[feature_name].mean()
+            # Find the standard deviation of the feature
+            feature_std = data[feature_name].std()
+            # Standardize the feature: Subtract the mean from the data point and divide by the standard deviation
+            data[feature_name] = (data[feature_name] - feature_mean)/feature_std
+
+    # Return the data
+    return data
+
+# Clustering(): Obtains the k-nearest neighbors
+def Clustering(trainData, testData):
+
+    # Create a dictionary for neighbors
+    neighbors = {}
+
+    # Iterate through the indices and rows in the test data
+    for test_index, test_row in testData.iterrows():
+        # Convert the features in the test data to a list
+        test_features = test_row[2:].tolist()
+        # Clear the neighbors list
+        neighbors.clear()
+        # Iterate through the indices and rows in the train data
+        for train_index, train_row in trainData.iterrows():
+            # Convert the features in the train data to a list
+            train_features = train_row[2:].tolist()
+            # Find the distance between the train and test features
+            distance = Distance(train_features, test_features)
+            # Extract the ID number of the training data row
+            index = train_row[0]
+            # Extract the classification of the training data row
+            classification = train_row[1]
+            # Set the distance in the neighbors dictionary
+            neighbors[(index, classification)] = distance
+
+# Distance(): Finds the Euclidean distance between the training and testing features
+def Distance(train_features, test_features):
+    return math.sqrt(sum((x - y) ** 2 for x, y in zip(train_features, test_features)))
+
+# Main()
+def main():
+
+    # Read the data from the csv file
+    df = pd.read_csv('data.csv')
+
+    # Drop the last row as it is meaningless
+    df.drop(df.columns[-1], axis=1, inplace=True)
+
+    # Perform one-hot encoding for the diagnosis categorical feature (B = 0, M = 1)
+    df["diagnosis"] = df["diagnosis"].replace('B', 0)
+    df["diagnosis"] = df["diagnosis"].replace('M', 1)
+
+     # Normalize the dataframe
+    data = Normalization(df)
+
+    # Set the training size
+    train_size = 0.7
+
+    # Call SplitData() to get the train and test data
+    trainData, testData = SplitData(data, train_size)
+
+    # Call Clustering() to obtain the k-nearest neighbors for each datapoint in the test dataset
+    Clustering(trainData, testData)
+
+if __name__=="__main__":
+    main()
