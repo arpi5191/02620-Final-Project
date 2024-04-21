c77058e0edbb0851b74b1a626d6d7bc3d82e03a5
Arpitha Rao <69826982+arpi5191@users.noreply.github.com>
2024-04-21 14:42:15 -0700

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -1,42 +1,14 @@
 # Import packages
 import math
+import numpy as np
 import pandas as pd
-
-# SplitData(): Splits the data into training and testing subsets (70-30)
-def SplitData(data, train_size):
-
-    # Shuffle the data
-    shuffled_data = data.sample(frac=1)
-
-    # Find the index up until which the training data will be extracted
-    trainInd = int(len(shuffled_data) * train_size)
-
-    # Obtain the training and testing data
-    train_data = shuffled_data[:trainInd]
-    test_data = shuffled_data[trainInd:]
-
-    # Return the training and testing data
-    return train_data, test_data
-
-# Normalization(): Normalize the data through standardization
-def Normalization(data):
-
-    # Iterate through the names and corresponding data for each feature
-    for feature_name, feature_data in data.items():
-        # Check if the feature is not id or diagnosis and if not apply the standardization
-        if feature_name != 'id' and feature_name != 'diagnosis':
-            # Find the mean of the feature
-            feature_mean = data[feature_name].mean()
-            # Find the standard deviation of the feature
-            feature_std = data[feature_name].std()
-            # Standardize the feature: Subtract the mean from the data point and divide by the standard deviation
-            data[feature_name] = (data[feature_name] - feature_mean)/feature_std
-
-    # Return the data
-    return data
+from sklearn.decomposition import PCA
+from PCA import find_PCs, PCA_transform
+from data_processing import trainD, testD
+from sklearn.preprocessing import StandardScaler
 
 # Clustering(): Obtains the k-nearest neighbors
-def Clustering(train_data, test_data, k):
+def Clustering(trainD, testD, k):
 
     # Create a dictionary for neighbors
     neighbors = {}
@@ -45,21 +17,21 @@ def Clustering(train_data, test_data, k):
     classifications = {}
 
     # Iterate through the indices and rows in the test data
-    for test_index, test_row in test_data.iterrows():
+    for test_index, test_row in testD.iterrows():
         # Convert the features in the test data to a list
         test_features = test_row[2:].tolist()
         # Clear the neighbors list
         neighbors.clear()
         # Iterate through the indices and rows in the train data
-        for train_index, train_row in train_data.iterrows():
+        for train_index, train_row in trainD.iterrows():
             # Convert the features in the train data to a list
             train_features = train_row[2:].tolist()
             # Find the distance between the train and test features
             distance = Distance(train_features, test_features)
             # Extract the ID number of the training data row
-            index = train_row[0]
+            index = train_row['id']
             # Extract the classification of the training data row
-            classification = train_row[1]
+            classification = train_row['diagnosis']
             # Set the distance in the neighbors dictionary
             neighbors[(index, classification)] = distance
         # Sort the neighbors dictionary by values (distance)
@@ -67,15 +39,15 @@ def Clustering(train_data, test_data, k):
         # Extract the first three neighbors
         selected_neighbors = dict(list(sorted_neighbors.keys())[:k])
         # Find the number of benign classifications that have occurred
-        zeroes_count = sum(1 for value in selected_neighbors.values() if value == 0)
+        zeroes_count = sum(1 for value in selected_neighbors.values() if value == float(0))
         # Find the number of malignant classifications that have occurred
-        ones_count = sum(1 for value in selected_neighbors.values() if value == 1)
+        ones_count = sum(1 for value in selected_neighbors.values() if value == float(1))
         # Check if the benign classifications is greater than the number of malignant classifications
         # If so assign the label as benign, otherwise malignant
         if zeroes_count > ones_count:
-            classifications[test_index] = 0
+            classifications[test_row[0]] = 0
         else:
-            classifications[test_index] = 1
+            classifications[test_row[0]] = 1
 
     return classifications
 
@@ -84,18 +56,19 @@ def Distance(train_features, test_features):
     return math.sqrt(sum((x - y) ** 2 for x, y in zip(train_features, test_features)))
 
 # Metrics(): Finds the metrics to evaluate the efficiency of the classifications
-def Metrics(test_data, classifications):
+def Metrics(testD, classifications):
 
     correct = 0
 
     # Iterate through the indices and rows in the test data
-    for test_index, test_row in test_data.iterrows():
+    for test_index, test_row in testD.iterrows():
+        test_id = test_row[0]
         # Convert the features in the test data to a list
         test_features = test_row[2:].tolist()
         # Obtain the actual classification from the row
         real_classification = test_row[1]
         # Obtain the predicted classifications from the clustering we performed
-        predicted_classification = classifications[test_index]
+        predicted_classification = classifications[test_id]
         # Check if the real classification is equal to the predicted classification
         # If so, increment # correct
         if real_classification == predicted_classification:
@@ -110,36 +83,69 @@ def Metrics(test_data, classifications):
 # Main()
 def main():
 
-    # Read the data from the csv file
-    df = pd.read_csv('data.csv')
-
-    # Drop the last row as it is meaningless
-    df.drop(df.columns[-1], axis=1, inplace=True)
-
-    # Perform one-hot encoding for the diagnosis categorical feature (B = 0, M = 1)
-    df["diagnosis"] = df["diagnosis"].replace('B', 0)
-    df["diagnosis"] = df["diagnosis"].replace('M', 1)
-
-     # Normalize the dataframe
-    data = Normalization(df)
-
-    # Set the training size
-    train_size = 0.7
-
-    # Call SplitData() to get the train and test data
-    train_data, test_data = SplitData(data, train_size)
-
-    # Set the number of k-nearest neighbors
     k = 3
 
-    # Call Clustering() to obtain the k-nearest neighbors for each datapoint in the test dataset
-    classifications = Clustering(train_data, test_data, k)
+    classifications = Clustering(trainD, testD, k)
 
-    # Call Metrics() to evaluate the accuracy of the KNN clustering
-    accuracy = Metrics(test_data, classifications)
+    accuracy = Metrics(testD, classifications)
 
-    # Print the accuracy
-    print("The accuracy of the KNN clustering is: {:.3f}%".format(accuracy))
+    print("The accuracy of the KNN clustering without PCA is: {:.3f}%".format(accuracy))
+
+    #---------------------------------------------------------------------------------------------------------------------
+
+    k = 7
+
+    num_PCs_needed, variance_covered, principle_components = find_PCs(trainD)
+    transformed_training_X = PCA_transform(trainD, principle_components)
+
+    column_names = [f'PC{i+1}' for i in range(num_PCs_needed)]
+
+    df_transformed_training_X = pd.DataFrame(transformed_training_X)
+    df_transformed_training_X.columns = column_names
+    df_transformed_training_X.insert(0, 'id', trainD['id'].values)
+    df_transformed_training_X.insert(1, 'diagnosis', trainD['diagnosis'].values)
+
+    num_PCs_needed, variance_covered, principle_components = find_PCs(testD)
+    transformed_testing_X = PCA_transform(testD, principle_components)
+
+    df_transformed_testing_X = pd.DataFrame(transformed_testing_X)
+    df_transformed_testing_X.columns = column_names
+    df_transformed_testing_X.insert(0, 'id', testD['id'].values)
+    df_transformed_testing_X.insert(1, 'diagnosis', testD['diagnosis'].values)
+
+    classifications = Clustering(df_transformed_training_X, df_transformed_testing_X, k)
+
+    accuracy = Metrics(df_transformed_testing_X, classifications)
+
+    print("The accuracy of the KNN clustering with PCA performed without packages is: {:.3f}%".format(accuracy))
+
+    #---------------------------------------------------------------------------------------------------------------------
+
+    k = 7
+
+    pca = PCA(n_components=8)
+
+    pca.fit(trainD)
+    train_pca = pca.transform(trainD)
+
+    df_transformed_training_X_pca = pd.DataFrame(train_pca)
+    df_transformed_training_X_pca.columns = column_names
+    df_transformed_training_X_pca.insert(0, 'id', trainD['id'].values)
+    df_transformed_training_X_pca.insert(1, 'diagnosis', trainD['diagnosis'].values)
+
+    pca.fit(testD)
+    test_pca = pca.transform(testD)
+
+    df_transformed_testing_X_pca = pd.DataFrame(test_pca)
+    df_transformed_testing_X_pca.columns = column_names
+    df_transformed_testing_X_pca.insert(0, 'id', testD['id'].values)
+    df_transformed_testing_X_pca.insert(1, 'diagnosis', testD['diagnosis'].values)
+
+    classifications = Clustering(df_transformed_training_X_pca, df_transformed_testing_X_pca, k)
+
+    accuracy = Metrics(df_transformed_testing_X_pca, classifications)
+
+    print("The accuracy of the KNN clustering with PCA performed with packages is: {:.3f}%".format(accuracy))
 
 if __name__=="__main__":
     main()
