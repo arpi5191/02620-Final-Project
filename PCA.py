ea2352f93d152f0fce1bf7c3d2abed8bd9f5ce0a
Arpitha Rao <69826982+arpi5191@users.noreply.github.com>
2024-04-30 23:12:03 -0700

    Update PCA.py
    Adding updated version of PCA

--- PCA.py
+++ PCA.py
@@ -13,30 +13,19 @@ def find_PCs(centered_data: np.array, variance_cover=0.9) -> (int, float, np.arr
     # Perform eigendecomposition to obtain the eigenvalues and eigenvectors
     eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix)
 
-    # Find the condition indices to identify the multicollinear features
-    condition_index = np.sqrt(eigenvalues.max() / eigenvalues)
-
-    # Utilize 50 as the threshold to filter out the multicollinear features
-    indices = [i for i, num in enumerate(condition_index) if num > 50]
-
-    # Filter out the multicollinear eigenvalues and eigenvectors
-    filtered_eigenvalues = [item for idx, item in enumerate(eigenvalues) if idx not in indices]
-    filtered_eigenvectors = [item for idx, item in enumerate(eigenvectors) if idx not in indices]
-
     # Find the sum of the eigenvectors
-    total_var = np.sum(filtered_eigenvalues)
+    total_var = np.sum(eigenvalues)
 
     # Initialize the following parameters
     PCs_needed = 1
-    PCs = filtered_eigenvectors[-1]
-    var_covered = filtered_eigenvalues[-1] / total_var
+    PCs = eigenvectors[-1]
+    var_covered = eigenvalues[-1] / total_var
 
     # Find the PCs and stack them in the matrix
-    # while var_covered < variance_cover:
-    for i in range(7):
+    while var_covered < variance_cover:
         PCs_needed += 1
-        PCs = np.row_stack((PCs, filtered_eigenvectors[-PCs_needed]))
-        var_covered += filtered_eigenvalues[-PCs_needed] / total_var
+        PCs = np.row_stack((PCs, eigenvectors[-PCs_needed]))
+        var_covered += eigenvalues[-PCs_needed] / total_var
 
     # Return the PCs needed, the variance covered and the PCs
     return PCs_needed, var_covered, PCs
@@ -49,3 +38,4 @@ def PCA_transform(centered_data: np.array, PCs: np.array) -> np.array:
 
     # Return the projected data
     return projected_data
+
